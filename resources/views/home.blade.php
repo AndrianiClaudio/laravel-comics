e86495e567a88bad4184b1f336e1d8f0c231c396
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-11 14:23:08 +0100

    Creazione repo. link css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+{{-- estendi layout base --}}
+@extends('guest.layout.base')
+{{-- title --}}
+@section('documentTitle')
+    Home
+@endsection
+{{-- content --}}
+@section('content')
+  <div>
+    home content
+  </div>
+@endsection
