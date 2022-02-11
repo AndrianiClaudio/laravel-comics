8e5871d4c8ceb43e5a5f76da22dc80fb2a20589e
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-11 15:39:50 +0100

    rotte per link header

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -1,3 +1,63 @@
 <header class="header">
-  Header
+  <div class="header-top">
+    <div class="main-container">
+      dc power visa
+      <select name="additional-sites-select" id="additional-sites-select">
+        <option value="additional-sites-option-0">ADDITIONAL DC SITES</option>
+      </select>
+    </div>
+  </div>
+  <div class="header-bottom">
+    <div class="main-container">
+      {{-- logo --}}
+      <img src="{{asset('img/dc-logo.png')}}" alt="Logo DC">
+      {{-- menu di navigazione --}}
+      <nav>
+        <ul>
+          <li>
+            <a href="{{route('characters')}}">
+              Characters
+            </a>
+          </li>
+          <li>
+            <a href="{{route('comics')}}">
+              comics
+            </a>
+          </li>
+          <li>
+            <a href="{{route('movies')}}">
+              movies
+            </a>
+          </li>
+          <li>
+            <a href="{{route('tv')}}">
+              tv
+            </a>
+          </li>
+          <li>
+            <a href="{{route('collectibles')}}">
+              collectibles
+            </a>
+          </li>
+          <li>
+            <a href="{{route('fans')}}">
+              fans
+            </a>
+          </li>
+          <li>
+            <a href="{{route('news')}}">
+              news
+            </a>
+          </li>
+          <li>
+            <a href="{{route('shop')}}">
+              shop
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {{-- input search --}}
+      <input type="text" name="input-search" id="input-search" placeholder="Search">
+    </div>
+  </div>
 </header>
