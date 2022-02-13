35996483fb2ac8a09990df404be57e1c6d36d798
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-13 12:11:55 +0100

    cols in footer-main dinamiche da file config

---
+++ config/footer.php
@@ -0,0 +1,47 @@
+<?php
+return [
+  'footer-main' => [
+    'cols' =>[
+      ['dc_comics' => [
+        'characters',
+        'comics',
+        'movies',
+        'TV',
+        'games',
+        'videos',
+        'news'
+      ],
+      'shop' => [
+      'shop DC',
+      'shop DC collectibles',
+      ],
+    ],
+    [
+      'dc' => [
+        'Terms Of Use',
+        'Privacy policy (New)',
+        'Ad Choices',
+        'Advertising',
+        'Jobs',
+        'Subscriptions',
+        'Talent Workshops',
+        'CPSC Certificates',
+        'Ratings',
+        'Shop Help',
+        'Contact us'
+      ]
+    ],
+    [
+      'sites' => [
+        'DC',
+        'MAD Magazine',
+        'DC kids',
+        'DC Universe',
+        'DC power'
+      ]
+    ],
+  ],
+  ],
+  'footer-bar' => [],
+];
+?>
--- resources/views/guest/partials/footer.blade.php
+++ resources/views/guest/partials/footer.blade.php
@@ -2,126 +2,24 @@
   <div class="footer-main">
     <div class="container">
       <div class="container-content">
-        {{-- colonna dc comics/shop --}}
-        <div class="col">
-          {{-- sezione dc comics --}}
-          <section>
-            <h2>DC COMICS</h2>
-            <ul>
-              <li>
-                <a href="{{route('characters')}}">
-                  Characters
-                </a>
-              </li>
-              <li>
-                <a href="{{route('comics')}}">
-                  Comics
-                </a>
-              </li>
-              <li>
-                <a href="{{route('movies')}}">
-                  Movies
-                </a>
-              </li>
-              <li>
-                <a href="{{route('tv')}}">
-                  TV
-                </a>
-              </li>
-              <li>
-                <a href="{{route('games')}}">
-                  Games
-                </a>
-              </li>
-              <li>
-                <a href="{{route('videos')}}">
-                  Videos
-                </a>
-              </li>
-              <li>
-                <a href="{{route('news')}}">
-                  News
-                </a>
-              </li>
-            </ul>
-          </section>
-          {{-- sezione shop --}}
-          <section>
-            <h2>SHOP</h2>
-            <ul>
-              <li>
-                <a href="#">Shop DC</a>
-              </li>
-              <li>
-                <a href="#">Shop DC Collectibles</a>
-              </li>
-            </ul>
-          </section>
-        </div>
-        {{-- colonna dc --}}
-        <div class="col">
-          <section>
-            <h2>DC</h2>
-            <ul>
-              <li>
-                <a href="#">Terms Of Use</a>
-              </li>
-              <li>
-                <a href="#">Privacy policy (New)</a>
-              </li>
-              <li>
-                <a href="#">Ad Choices</a>
-              </li>
-              <li>
-                <a href="#">Advertising</a>
-              </li>
-              <li>
-                <a href="#">Jobs</a>
-              </li>
-              <li>
-                <a href="#">Subscriptions</a>
-              </li>
-              <li>
-                <a href="#">Talent Workshops</a>
-              </li>
-              <li>
-                <a href="#">CPSC Certificates</a>
-              </li>
-              <li>
-                <a href="#">Ratings</a>
-              </li>
-              <li>
-                <a href="#">Shop Help</a>
-              </li>
-              <li>
-                <a href="#">Contact us</a>
-              </li>
-            </ul>
-          </section>
-        </div>
-        {{-- colonna sites --}}
-        <div class="col">
-          <section>
-            <h2>SITES</h2>
-            <ul>
-              <li>
-                <a href="#">DC</a>
-              </li>
-              <li>
-                <a href="#">MAD Magazine</a>
-              </li>
-              <li>
-                <a href="#">DC Kids</a>
-              </li>
-              <li>
-                <a href="#">DC Universe</a>
-              </li>
-              <li>
-                <a href="#">DC Power Visa</a>
-              </li>
-            </ul>
-          </section>
-        </div>
+        @foreach (config('footer.footer-main.cols') as $cols)
+            <div class="col">
+              @foreach ($cols as $i => $c)
+              <section>
+                <h2>{{str_replace('_',' ',$i)}}</h2>
+                <ul>
+                  @foreach($c as $item)
+                  <li>
+                    <a href="#">{{ucfirst($item)}}</a>
+                  </li>
+                  @endforeach
+                </ul>
+              </section>
+              @endforeach
+            </div>
+        @endforeach
+
+
         <div class="container">
           <p>All Site Content TM and &COPY; 2020 DC Entertainment&comma; unless otherwhise <a class="blue-color">noted here</a>. All rights reserved.</p>
           <a class="blue-color">Cookie Settings</a>
