5f97f9bb50f5a37aac2e80201b664d6d1db1cfc9
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-14 15:36:13 +0100

    impaginazione sub card container

--- resources/views/guest/layout/card_sub.blade.php
+++ resources/views/guest/layout/card_sub.blade.php
@@ -3,9 +3,10 @@
     <div class="container-content">
       <section>
         <h2>Talent</h2>
-        <ul>
+        <ul class="list">
           <li class="list-item">
-            <label>Art by:</label>
+            <span class='title-txt'>Art by:</span>
+            <div class="item-txt">
             @foreach ($card['artists'] as $art)
                 <a href="#">
                   {{$art}}
@@ -14,43 +15,56 @@
                   @endif
                 </a>
             @endforeach
+            </div>
           </li>
           <li class="list-item">
-            <span>Art by:</span>
-            @foreach ($card['writers'] as $wri)
-                <a href="#">
-                  {{$wri}}
-                  @if ($wri !== last($card['writers']))
-                      ,
-                  @endif
-                </a>
-            @endforeach
+            <span class='title-txt'>Written by:</span>
+            <div class="item-txt">
+              @foreach ($card['writers'] as $wri)
+              <a href="#">
+                {{$wri}}
+                @if ($wri !== last($card['writers']))
+                ,
+                @endif
+              </a>
+              @endforeach
+            </div>
           </li>
         </ul>
       </section>
       <section>
         <h2>Specs</h2>
-        <ul>
+        <ul class="list">
           <li class="list-item">
-            <span>Series:</span>
-            <a href="#">
-              {{$card['series']}}
-            </a>
+            <span class='title-txt'>Series:</span>
+            <div class="item-txt">
+              <a href="#">
+                {{$card['series']}}
+              </a>
+            </div>
           </li>
           <li class="list-item">
-            <span>U.S. Price:</span>
-            <a href="#">
-              {{$card['price']}}
-            </a>
+            <span class='title-txt'>U.S. Price:</span>
+            <div class="item-txt">
+              <span>
+                {{$card['price']}}
+              </span>
+            </div>
           </li>
           <li class="list-item">
-            <span>On Sale Date: </span>
-            <a href="#">
-              {{$card['sale_date']}}
-            </a>
+            <span class='title-txt'>On Sale Date: </span>
+            <div class="item-txt">
+              <span>
+                {{$card['sale_date']}}
+              </span>
+            </div>
           </li>
         </ul>
       </section>
+    </div>
+    {{-- barra icone (in unico file img gestito con css(_card.info))) --}}
+    <div class="link-bar">
+
     </div>
   </div>
 </div>
