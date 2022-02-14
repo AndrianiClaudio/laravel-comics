3df7c7faa1df5d98f4d36628dac0afaa19240445
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-02-14 15:13:12 +0100

    main_card_info_content

--- resources/views/guest/layout/card.blade.php
+++ resources/views/guest/layout/card.blade.php
@@ -3,9 +3,23 @@
     <div class="container-content">
       @yield('absolute')
       <div class="container">
-        <h2>{{$card['title']}}</h2>
-        {{$card['price']}} check avaibi...
-        <p>{{$card['description']}}</p>
+        <h1 class="title">{{$card['title']}}</h1>
+        <div class="price-available">
+          <div class="price">
+            <label for="price-value">U.S. Price: 
+            <span class="price-value" id="price-value">{{$card['price']}}</span></label>
+            <span>AVAILABLE</span>
+          </div>
+          <div class="availability">
+            <span>Check Availability</span>
+            <i class="fa-solid fa-sort-down"></i>
+          </div>
+        </div>
+        <p class="description">{{$card['description']}}</p>
+      </div>
+      <div class="adv">
+        <span class="title">advertisement</span>
+        <img src="{{asset('img/adv.jpg')}}" alt="advertisement">
       </div>
     </div>
   </div>
